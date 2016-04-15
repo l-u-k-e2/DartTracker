97ad6257f6b4a3a80308f2a919616ce15b004ee6
Lukas <sclukasmuellerbs@gmail.com>
2016-04-15 15:09:10 +0200

    Absprung auf newGame Activity

--- app/src/main/java/com/ostfalia/bs/darttracker/MainActivity.java
+++ app/src/main/java/com/ostfalia/bs/darttracker/MainActivity.java
@@ -1,13 +1,24 @@
 package com.ostfalia.bs.darttracker;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button)findViewById(R.id.button_new_game);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, NewGame.class));
     }
 }
---
+++ app/src/main/java/com/ostfalia/bs/darttracker/NewGame.java
@@ -0,0 +1,9 @@
+package com.ostfalia.bs.darttracker;
+
+import android.app.Activity;
+
+/**
+ * Created by Lukas on 15.04.2016.
+ */
+public class NewGame extends Activity {
+}
