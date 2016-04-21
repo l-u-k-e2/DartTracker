bd8d49b10667e042e1cef6c86b340508c25e6e6e
lukas <sclukasmuellerbs@gmail.com>
2016-04-21 18:59:29 +0200

    TableView hinzugefügt

--- app/src/main/java/com/ostfalia/bs/darttracker/fragment/ScoringFragment.java
+++ app/src/main/java/com/ostfalia/bs/darttracker/fragment/ScoringFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.ostfalia.bs.darttracker.R;
@@ -28,8 +30,12 @@ public class ScoringFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
         ArrayList<Integer> arrayList = args.getIntegerArrayList("user");
-        TextView text = (TextView) getActivity().findViewById(R.id.textView2);
-        text.setText(text.getText() + arrayList.get(0).toString());
+        TableLayout tableLayout = (TableLayout)getActivity().findViewById(R.id.table_layout_scoring);
+        TableRow tableRow = new TableRow(getActivity());
+        TextView playerName = new TextView(getActivity());
+        playerName.setText(arrayList.get(0).toString());//hier muss Name aus DB geholt werden
+        tableRow.addView(playerName);
+        tableLayout.addView(tableRow);
         super.onActivityCreated(savedInstanceState);
     }
 }
